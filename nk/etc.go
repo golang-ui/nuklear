a384a8fd1ab32e565931422fff774145f895a6e8
Your Name <you@example.com>
2018-11-13 17:31:16 +0100

    added setters and getters for spacing, padding & grouppadding

--- nk/etc.go
+++ nk/etc.go
@@ -301,11 +301,35 @@ func (s *Style) Text() *StyleText {
 }
 
 func (s *Style) Window() *StyleWindow {
-    return (*StyleWindow)(&s.window)
+	return (*StyleWindow)(&s.window)
 }
 
 func (w *StyleWindow) Background() *Color {
 	return (*Color)(&w.background)
+}
+
+func (w *StyleWindow) Spacing() *Vec2 {
+	return (*Vec2)(&w.spacing)
+}
+
+func (w *StyleWindow) Padding() *Vec2 {
+	return (*Vec2)(&w.padding)
+}
+
+func (w *StyleWindow) GroupPadding() *Vec2 {
+	return (*Vec2)(&w.group_padding)
+}
+
+func SetSpacing(ctx *Context, v Vec2) {
+	*ctx.Style().Window().Spacing() = v
+}
+
+func SetPadding(ctx *Context, v Vec2) {
+	*ctx.Style().Window().Padding() = v
+}
+
+func SetGroupPadding(ctx *Context, v Vec2) {
+	*ctx.Style().Window().GroupPadding() = v
 }
 
 func SetTextColor(ctx *Context, color Color) {
